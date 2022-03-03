9be35936a7566760d8225e0f880903900a37d2cf
acheatham87 <acheatham87@gmail.com>
2022-03-03 14:35:49 -0600

    added extra city and landscape

--- scripts/Ireland/irelandData.js
+++ scripts/Ireland/irelandData.js
@@ -6,14 +6,22 @@ const cities = [
     {
         name:"Dublin",
         pop:"1,255,963",
-        url:"https://en.wikipedia.org/wiki/Dublin"
+        url:"https://en.wikipedia.org/wiki/Dublin",
+        knownFor: "The Guiness Storehouse"
     },
     
     {
         name:"Belfast",
         pop:"638,717",
-        url:"https://en.wikipedia.org/wiki/Belfast"
+        url:"https://en.wikipedia.org/wiki/Belfast",
+        knownFor: "Creation of the RMS Titanic" 
     },
+    {
+        name:"Cork",
+        pop:"190,384",
+        url:"https://en.wikipedia.org/wiki/Cork_(city)",
+        knownFor:  "The first potato was planted here"
+    }
     
 ]
 
@@ -21,12 +29,20 @@ const landmarks = [
     {
         name: "Cliffs of Moher",
         location: "County Clare",
-        url: "https://en.wikipedia.org/wiki/Cliffs_of_Moher"
+        url: "https://en.wikipedia.org/wiki/Cliffs_of_Moher",
+        created: "320 million years ago."
     },
     {
         name: "St. Patrick’s Cathedral",
         location:  "Dublin",
-        url: "https://en.wikipedia.org/wiki/St._Patrick%27s_Cathedral"
+        url: "https://en.wikipedia.org/wiki/St._Patrick%27s_Cathedral",
+        created: "1191"
+    },
+    {
+        name: "Rock of Cashel",
+        location:  "Tipperary",
+        url: "https://en.wikipedia.org/wiki/Rock_of_Cashel",
+        created: "the 12th Century"
     }
 ]
 
@@ -34,13 +50,13 @@ const celebs = [
     {
         name: "C.S. Lewis",
         occupation: "Writer",
-        description: "(29 November 1898 – 22 November 1963) was a British writer and lay theologian. He is best known for his works of fiction, especially The Screwtape Letters, The Chronicles of Narnia, and The Space Trilogy.",
+        description: "A British writer and lay theologian. Best known for his works of fiction, especially The Screwtape Letters, The Chronicles of Narnia, and The Space Trilogy.",
         page:"https://en.wikipedia.org/wiki/C._S._Lewis"
     },
     {
         name: "Angela Lansbury",
         occupation: "Actress / Singer",
-        description: "(born 16 October 1925) is an Irish-British[2] actress and singer who has played many film, theater, and television roles. Her career has spanned over 80 years. Lansbury is one of the last surviving stars from the Golden Age of Hollywood cinema.",
+        description: "An Irish-British actress and singer who has played many film, theater, and television roles. Lansbury is one of the last surviving stars from the Golden Age of Hollywood cinema.",
         page:"https://en.wikipedia.org/wiki/Angela_Lansbury"
     },
 ]
