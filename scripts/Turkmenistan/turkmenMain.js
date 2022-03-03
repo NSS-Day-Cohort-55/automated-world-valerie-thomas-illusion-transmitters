ff4d72458c523d533ee39228e98f72dcaf7ea787
Hulikner <hulikner@gmail.com>
2022-03-03 10:25:05 -0600

    Turkmenistan

---
+++ scripts/Turkmenistan/turkmenMain.js
@@ -0,0 +1,8 @@
+//import
+
+import { turkmenCelebLister, turkmenCityLister, turkmenLandmarkLister } from "./turkmenList.js";
+
+
+turkmenCityLister();
+turkmenLandmarkLister();
+turkmenCelebLister();
